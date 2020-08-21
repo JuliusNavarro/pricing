eb62875854a02bef18a78d4cdb18e69c05032062
JuliusNavarro <68299495+JuliusNavarro@users.noreply.github.com>
2020-08-22 02:55:14 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $(".generalItems").click(function(){
+      $(".general").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  });
+
+  $(document).ready(function(){
+    $(".funnelItems").click(function(){
+      $(".funnel").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  });
+
+  $(document).ready(function(){
+    $(".pbItems").click(function(){
+      $(".prodBuilder").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  });
+
+  $(document).ready(function(){
+    $(".mbItems").click(function(){
+      $(".memBuilder").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  });
+
+  $(document).ready(function(){
+    $(".lbItems").click(function(){
+      $(".lstBuilder").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  }); 
+  
+  $(document).ready(function(){
+    $(".blogItems").click(function(){
+      $(".blogBuilder").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  });
+
+  $(document).ready(function(){
+    $(".afItems").click(function(){
+      $(".afCenter").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  });
+
+  $(document).ready(function(){
+    $(".suppItems").click(function(){
+      $(".suppDesk").toggle();
+      icon = $(this).find("i");
+      icon.toggleClass("fa fa-angle-down fa fa-angle-up")
+    });
+  });
